1bc5ce481672e06fd31dff0787badafca0c3a8ef
Liuban0Stanislav <liubanstanislav@yahoo.com>
2023-07-25 18:03:31 +1000

    ДЗ 3.4

--- src/main/java/ru/hogwarts/school/service/StudentService.java
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -19,7 +19,6 @@ public class StudentService {
 
     public Student createStudent(Student student) {
         return studentRepository.save(student);
-
     }
 
     public Student findStudent(Long id) {
